08709d58a4e6adab71d754a8f09c49445d72b16f
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:25:20 +0100

    Add project files.

---
+++ OpenLab 02.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenLab_02._3
+{
+    class Program
+    {
+        public static int Remainder(int a, int b)
+        {
+            return (a % b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadaj prvé číslo :");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadaj druhé číslo :");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Výsledok delenia je :");
+            Console.WriteLine(Remainder(a, b));
+
+        }
+    }
+}
